20dd0248b210589e32e5e60d79151169048d673c
mushirih <pmwachira@gmail.com>
2016-05-17 11:19:15 +0300

    17/05/2016

--- app/src/main/java/mushirih/thoughtleadership2/Events.java
+++ app/src/main/java/mushirih/thoughtleadership2/Events.java
@@ -2,6 +2,7 @@ package mushirih.thoughtleadership2;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -91,22 +92,41 @@ public class Events extends AppCompatActivity
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public void onItemClick(View view, EventItem item) {
-       // Log.d("STATUS", "Av CURRENTLY PRESSED "+item);
-        //CHECK SYNCRONY OF THIS
+
+      //  saveEve(item);
+        //OR
+        showEventNow(item);
+    }
+
+    private void saveEve(EventItem item) {
         Calendar beginTime = Calendar.getInstance();
 
         beginTime.set(Integer.parseInt(item.getYear()), Integer.parseInt(item.getMonth())-1, Integer.parseInt(item.getDate()));
         Intent calendar=new Intent(Intent.ACTION_INSERT);
-         //calendar.setData(CalendarContract.Events.CONTENT_URI);
+        //calendar.setData(CalendarContract.Events.CONTENT_URI);
         calendar.setType("vnd.android.cursor.item/event");
         calendar.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
-       // calendar.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+        // calendar.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
         calendar.putExtra(CalendarContract.Events.TITLE,item.getTitle());
         //calendar.putExtra(CalendarContract.Events.DESCRIPTION,item.getDesc()+"\n\nFor Locations Ask : "+item.getOwner());
         calendar.putExtra(CalendarContract.Events.EVENT_LOCATION,"Ask "+item.getOwner());
         //calendar.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         calendar.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         getApplicationContext().startActivity(calendar);
+    }
+
+    private void showEventNow(final EventItem item) {
+        new android.support.v7.app.AlertDialog.Builder(this).setIcon(R.drawable.icon).setTitle("Event name: "+item.getTitle()).setMessage(item.getDesc()+" on "+item.getDate()+"/"+item.getMonth()+"/"+item.getYear()).setPositiveButton("Save Event?", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               saveEve(item);
+            }
+        }).setNegativeButton("Don't save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).show();
     }
 
     @Override
