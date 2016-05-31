35ee5f50f4d09ed9c419d27d638f5fc39d5aab84
mushirih <pmwachira@gmail.com>
2016-05-31 11:52:18 +0300

    31/05/2016-1

--- app/src/main/java/mushirih/thoughtleadership2/Splash.java
+++ app/src/main/java/mushirih/thoughtleadership2/Splash.java
@@ -1,17 +1,14 @@
 package mushirih.thoughtleadership2;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.VideoView;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by p-tah on 25/01/2016.
  */
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
 
     Handler handler=new Handler();
     Runnable runnable=new Runnable() {
@@ -25,39 +22,38 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);/*
+
         setContentView(R.layout.splash);
-        ImageView splash= (ImageView) findViewById(R.id.imagesplash);
-        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise);
-        splash.startAnimation(animation);
-        handler.postDelayed(runnable,3000);*/
+//        ImageView splash= (ImageView) findViewById(R.id.imagesplash);
+//        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise);
+//        splash.startAnimation(animation);
+        handler.postDelayed(runnable, 3000);
 
-         VideoView videoHolder = (VideoView) findViewById(R.id.spl);
-
-        try{
-  Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
-          + R.raw.splashh);
-
-  videoHolder.setVideoURI(video);
-   videoHolder.requestFocus();
-            videoHolder.setZOrderOnTop(true);
-  videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-      public void onCompletion(MediaPlayer mp) {
-          Intent newIntent = new Intent(getBaseContext(), Home.class);
-
-          startActivity(newIntent);
-      }
-
-  });
-
-  videoHolder.start();
- } catch(Exception ex) {
-     Intent newIntent=new Intent(getBaseContext(), Home.class);
-     startActivity(newIntent);
- }
-    }
-
+//         VideoView videoHolder = (VideoView) findViewById(R.id.spl);
+//
+//        try{
+//  Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
+//          + R.raw.splashh);
+//
+//  videoHolder.setVideoURI(video);
+//   videoHolder.requestFocus();
+//            videoHolder.setZOrderOnTop(true);
+//  videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//
+//      public void onCompletion(MediaPlayer mp) {
+//          Intent newIntent = new Intent(getBaseContext(), Home.class);
+//
+//          startActivity(newIntent);
+//      }
+//
+//  });
+//
+//  videoHolder.start();
+// } catch(Exception ex) {
+//     Intent newIntent=new Intent(getBaseContext(), Home.class);
+//     startActivity(newIntent);
+// }
+}
     @Override
     protected void onPause() {
         super.onPause();
